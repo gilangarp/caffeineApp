c202ca38a5e5b0db5336244e57fc0314c0f919ae
gilangarp <gilangarya2001.ap@gmail.com>
2024-11-21 22:54:08 +0700

    Install redux and react-redux for state management

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,15 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import {  RouterProvider } from 'react-router-dom'
-import { Router } from './Router'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { Router } from "./Router";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import React from "react";
 
-createRoot(document.getElementById('root')!).render(
-    <RouterProvider router={Router} /> 
-)
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={Router} />
+    </React.StrictMode>
+  </Provider>
+);

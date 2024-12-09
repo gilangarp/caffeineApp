3d7888de0ea721090e41554248f6170312bf24d6
gilangarp <gilangarya2001.ap@gmail.com>
2024-12-09 15:12:56 +0700

    Fix Tailwind ESM issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,8 @@ module.exports = {
         'primary': '#FF8906',
       },
       fontSize: {
-        'heading_mobile': ['32px','28px'],
-        'heading_desktop': ['45px','38px']
+        'heading_mobile': ['32px', '28px'],
+        'heading_desktop': ['45px', '38px']
       },
       backgroundImage: {
         'gradient-to-r': 'linear-gradient(168.18deg, #777C82 -114.74%, #0B0909 91.35%)',

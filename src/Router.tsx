874fcd7600355ea72af4ffda6cf6fefe7587fdf8
gilangarp <gilangarya2001.ap@gmail.com>
2024-12-16 11:48:24 +0700

    update router to v7

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import { UserLayout } from "./app/layout/UserLayout";
 import { HomePage } from "./app/home/HomePage";
 import { ProductPage } from "./app/product/ProductPage";
@@ -15,87 +15,63 @@ import { ErrorPage } from "./app/Error/ErrorPage";
 import { UnauthorizedPage } from "./app/Error/UnauthorizedPage";
 
 export const Router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <UserLayout />,
-      errorElement: <ErrorPage />,
-      children: [
-        {
-          path: "",
-          element: <HomePage />,
-        },
-        {
-          path: "/product",
-          element: <ProductPage />,
-        },
-        {
-          path: "/dashboard",
-          element: (
-            <PrivateRoute to={"/login"} requiredRoles={['admin']}>
-              <DashboardPage />
-            </PrivateRoute>
-          ),
-        },
-        {
-          path: "/detail-product/:id",
-          element: <ProductDetailPage />,
-        },
-        {
-          path: "/checkout",
-          element: (
-            <PrivateRoute to={"/login"} requiredRoles={['user']}>
-              <CheckoutPage />
-            </PrivateRoute>
-          ),
-        },
-        {
-          path: "/history-order",
-          element: <HistoryOrderPage />,
-        },
-        {
-          path: "/profile",
-          element: (
-            <PrivateRoute to={"/login"} requiredRoles={['user']}>
-              <ProfilePage />
-            </PrivateRoute>
-          ),
-        },
-        {
-          path: "/detail-order",
-          element: (
-            <PrivateRoute to={"/login"} requiredRoles={['user']}>
-              <HistoryOrderPage />
-            </PrivateRoute>
-          ),
-        },
-        {
-          path: "/order/:id",
-          element: (
-            <PrivateRoute to={"/login"} requiredRoles={['user']}>
-              <DetailHistoryOrderPage />
-            </PrivateRoute>
-          ),
-        },
-      ],
-    },
-    {
-      path: "/login",
-      element: <LoginPage />,
-    },
-    {
-      path: "/register",
-      element: <RegisterPage />,
-    },
-    {
-      path: "/unauthorized",
-      element: <UnauthorizedPage />,
-    },
-  ],
-  {
-    future: {
-      v7_skipActionErrorRevalidation: true,
-      v7_startTransition: true,
-    },
-  }
+  createRoutesFromElements(
+    <>
+    <Route path="/" element={<UserLayout />} errorElement={<ErrorPage />}>
+      <Route index element={<HomePage />} />
+      <Route path="product" element={<ProductPage />} />
+      <Route
+        path="dashboard"
+        element={
+          <PrivateRoute to="/login" requiredRoles={['admin']}>
+            <DashboardPage />
+          </PrivateRoute>
+        }
+      />
+      <Route path="detail-product/:id" element={<ProductDetailPage />} />
+      <Route
+        path="checkout"
+        element={
+          <PrivateRoute to="/login" requiredRoles={['user']}>
+            <CheckoutPage />
+          </PrivateRoute>
+        }
+      />
+      <Route path="history-order" element={<HistoryOrderPage />} />
+      <Route
+        path="profile"
+        element={
+          <PrivateRoute to="/login" requiredRoles={['user']}>
+            <ProfilePage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="detail-order"
+        element={
+          <PrivateRoute to="/login" requiredRoles={['user']}>
+            <HistoryOrderPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="order/:id"
+        element={
+          <PrivateRoute to="/login" requiredRoles={['user']}>
+            <DetailHistoryOrderPage />
+          </PrivateRoute>
+        }
+      />
+    </Route>
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/register" element={<RegisterPage />} />
+    <Route path="/unauthorized" element={<UnauthorizedPage />} />
+  </>
+),
+{
+  future: {
+    v7_skipActionErrorRevalidation: true,
+    v7_startTransition: true,
+  },
+}
 );
